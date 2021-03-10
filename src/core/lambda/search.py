1b6972cb69ba2412915aca4f25563090aae4183a
Joao Koritar <jakoritarleite@gmail.com>
2021-03-10 15:04:09 -0300

    Get XPath from database for search function

--- src/core/lambda/search.py
+++ src/core/lambda/search.py
@@ -4,19 +4,8 @@ import src.modules.url as URL
 import src.modules.crawl as crawl
 import src.modules.header as Header
 import src.modules.request as request
+import src.modules.database as database
 import src.modules.exceptions as exceptions
-
-xpaths = {
-    "domain": "anageimoveis.com.br",
-    "id": "288864e4-553a-4094-99a4-3b3934ea219f",
-    "parser_arguments_render": "false",
-    "parser_arguments_wait": "true",
-    "parser_arguments_wait_time": "30.0",
-    "parser_items": "//*[@id='block-result-search']/section/div[@class='cards-list']//article[contains(@class,'bloco')]",
-    "parser_items_url": ".//span[contains(@class,'img')]/a[contains(@class,'lk-img')]/@href",
-    "parser_next_page": "//*[@id=\"block-result-search\"]/section//span/a[text()='Próxima página']/@href",
-    "parser_url_string": "page="
-}
 
 def handler(event: dict, context) -> dict:
     url: str = event.get('url')
@@ -26,6 +15,12 @@ def handler(event: dict, context) -> dict:
     
     if not URL.validator(url):
         raise exceptions.NoValidURL("You must provide a valide URL to lambda:search")
+
+    xpaths = database.query('SEARCH_CONFIG_TABLE', {
+        'index': 'domain-index',
+        'key': 'domain',
+        'value': URL.extract_domain(url)
+    })[0]
 
     header = Header.Header()
     response = request.Request(
