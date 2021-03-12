97408423e7e96721aa5bd6554300a7db7d548903
Joao Koritar <jakoritarleite@gmail.com>
2021-03-12 15:45:37 -0300

    Add query using sort key

--- src/modules/database/get.py
+++ src/modules/database/get.py
@@ -36,14 +36,32 @@ def scan(table: str, query: dict = dict()) -> list:
         raise DynamoDB.ClientError(f'Error when scanning Dynamo {table} with query {query}\n\n{error}')
 
     else:
-        return result
+        if result: 
+            return result[0]
+        else: 
+            return dict()
 
-def query(table: str, query: dict) -> list:
+def query(table: str, query: dict) -> dict:
     """Query the desired table using kwargs
     
     Args:
         param1 (str) table: The DynamoDB Table name
         param2 (dict) query: The target query options
+            {
+                index: The Table GSI,
+                key: The primary key of the index,
+                value: The value of the key to query on the Table
+            }
+
+            or 
+
+            {
+                index: The Table GSI,
+                hash_key: The primary key of the index,
+                hash_value: The value of the primary key to query on the Table
+                sort_key: The sort key of the index,
+                sort_value: The value of the sort key to query on the Table
+            }
 
     Return:
         The response Items from DynamoDb which matches the query
@@ -57,9 +75,14 @@ def query(table: str, query: dict) -> list:
     dynamodb_table = dynamodb_client.Table(os.environ.get(table))
 
     query_kwargs = {
-        'IndexName': query.get('index'),
-        'KeyConditionExpression': conditions.Key(query.get('key')).eq(query.get('value'))
+        'IndexName': query.get('index')
     }
+
+    if query.get('sort_key'):
+        query_kwargs['KeyConditionExpression'] = conditions.Key(query.get('hash_key')).eq(query.get('hash_value')) & conditions.Key(query.get('range_key')).eq(query.get('range_value'))
+
+    else:
+        query_kwargs['KeyConditionExpression'] = conditions.Key(query.get('key')).eq(query.get('value'))
 
     try:
         while not done:
@@ -74,4 +97,7 @@ def query(table: str, query: dict) -> list:
         raise DynamoDB.ClientError(f'Error when quering Dynamo {table} with query {query}\n\n{error}')
 
     else:
-        return result
+        if result:
+            return result[0]
+        else:
+            return dict()
