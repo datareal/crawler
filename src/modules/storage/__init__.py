eb6163b6a6cea97b5223d49f6b433dbb352b2f1f
Joao Koritar <jakoritarleite@gmail.com>
2021-03-15 18:05:11 -0300

    storage: Add function to split s3_uri into bucket, path and filename

--- src/modules/storage/__init__.py
+++ src/modules/storage/__init__.py
@@ -1,1 +1,5 @@
-from .get import download
+from .get import download, \
+                filename_from_url, \
+                split_s3_uri
+
+from .save import save
--- src/modules/storage/get.py
+++ src/modules/storage/get.py
@@ -1,6 +1,8 @@
+import os
 import boto3
 import hashlib
 import datetime
+import urllib.parse
 from botocore import exceptions
 
 import src.modules.url as URL
@@ -17,23 +19,23 @@ def download(bucket: str, path: str, filename: str) -> bytes:
         If the object exist it return True, and if not it return False
     """
     client = boto3.resource('s3')
+    object_key = f'{path}/{filename}'
     response: bytes
 
     try:
-        obj = client.Object(bucket_name=bucket, key=f'{path}/{filename}').get()
+        obj = client.Object(bucket_name=bucket, key=object_key).get()
 
     except exceptions.ClientError as error:
         if error.response['Error']['Code'] == 'NoSuchKey':
-            response = bytes(0)
+            response = bytes(0x0001)
 
         else:
             print(error)
             raise Exception('Something went wrong searching for file in S3 Bucket.')
     else:
-        response = obj['body'].read()
+        response = obj['Body'].read()
     
     return response
-
 
 def filename_from_url(url: str) -> dict:
         """Create the S3 object filename from the given URL
@@ -54,3 +56,21 @@ def filename_from_url(url: str) -> dict:
             'folder': f'{folder}/{file}',
             'file': datetime.datetime.today().strftime("%Y-%m-%d") + '.body'
         }
+
+def split_s3_uri(uri: str) -> tuple:
+    """This splits the S3 uri to get the bucket, domain, path and fsilename
+
+    Args:
+        param1 (str) uri: The S3 URI
+
+    Returns:
+        A tuple containing the S3 bucket, hashed path and the filename
+    """
+    parsed_uri = urllib.parse.urlparse(uri)
+    parsed_path = os.path.split(parsed_uri.path)
+
+    bucket = parsed_uri.netloc
+    path = parsed_path[0][1:] # The 1: is required to remove the "/" from the start of the string
+    filename = parsed_path[1]
+
+    return (bucket, path, filename)
